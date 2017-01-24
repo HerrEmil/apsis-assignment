1e3966bb5419b8e2f1d9d39a0703c1761223fd49
Emil Andersson <vaccuum@gmail.com>
2017-01-24 20:56:01 +0100

    Add set of ten frames and basic server/client communication

---
+++ public/app.js
@@ -0,0 +1,50 @@
+var app = angular.module('bowlingApp', []);
+
+app.controller('GameController', function($http) {
+	var game = this;
+
+	game.frames = [{
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: ''
+	}, {
+		first: '',
+		second: '',
+		bonus: ''
+	}];
+
+	game.result = 0;
+
+	game.calculate = function() {
+		$http.post('/calculate-bowling-score', game.frames)
+			.then(function successCallback(response) {
+				game.result = response.data;
+			}, function errorCallback(response) {
+				console.log('error:');
+				console.log(response);
+			});
+	};
+});
